a2caf1914c61810a6b1edf4b5e1475f9de5e14aa
Kuliza-233 <bebi2527@live.in>
2018-06-01 13:48:05 +0530

    feat: Added Junit tests for HomeViewmodel

---
+++ app/src/test/java/com/iambedant/mvistarter/home/HomeViewModelTest.kt
@@ -0,0 +1,117 @@
+package com.iambedant.mvistarter.home
+
+import android.arch.core.executor.testing.InstantTaskExecutorRule
+import android.arch.lifecycle.Observer
+import com.iambedant.mvistarter.data.Repository
+import com.iambedant.mvistarter.data.remote.model.ArticlesItem
+import com.iambedant.mvistarter.data.remote.model.NewsResponse
+import com.iambedant.mvistarter.data.remote.model.Source
+import com.iambedant.mvistarter.feature.home.HomeActionProcessorHolder
+import com.iambedant.mvistarter.feature.home.HomeIntent
+import com.iambedant.mvistarter.feature.home.HomeViewModel
+import com.iambedant.mvistarter.feature.home.HomeViewState
+import com.iambedant.mvistarter.feature.login.LoginActionProcessorHolder
+import com.iambedant.mvistarter.feature.login.LoginViewModel
+import com.iambedant.mvistarter.feature.login.LoginViewState
+import com.iambedant.mvistarter.util.schedulers.BaseSchedulerProvider
+import com.iambedant.mvistarter.util.schedulers.ImmediateSchedulerProvider
+import com.nhaarman.mockito_kotlin.times
+import com.nhaarman.mockito_kotlin.verify
+import com.nhaarman.mockito_kotlin.whenever
+import io.reactivex.Observable
+import io.reactivex.Single
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.TestRule
+import org.mockito.Mock
+import org.mockito.MockitoAnnotations
+import org.mockito.verification.VerificationMode
+
+/**
+ * Created by @iamBedant on 01/06/18.
+ */
+class HomeViewModelTest {
+    @Mock
+    private lateinit var repository: Repository
+    private lateinit var schedulerProvider: BaseSchedulerProvider
+    private lateinit var homeViewModel: HomeViewModel
+    @Mock
+    lateinit var observer: Observer<HomeViewState>
+
+    @get:Rule
+    var rule: TestRule = InstantTaskExecutorRule()
+
+    @Before
+    fun setUpLoginViewModel() {
+        MockitoAnnotations.initMocks(this)
+        schedulerProvider = ImmediateSchedulerProvider()
+        homeViewModel = HomeViewModel(HomeActionProcessorHolder(repository, schedulerProvider))
+        homeViewModel.states().observeForever(observer)
+    }
+
+    @Test
+    fun InitialIntentTest() {
+        val articles = listOf<ArticlesItem>()
+        whenever(repository.loadNews()).thenReturn(Single.just(NewsResponse(
+                totalResults = 10,
+                articles = articles,
+                status = ""
+        )))
+        homeViewModel.processIntents(Observable.just(HomeIntent.InitialIntent))
+        verify(observer).onChanged(HomeViewState(
+                isLoading = true,
+                isError = false,
+                errorMessage = "",
+                articles = emptyList(),
+                showShareOption = false,
+                shareArticle = null
+        ))
+        //TODO: figure out why this viewstate in invoked twice
+        verify(observer, times(2)).onChanged(HomeViewState(
+                isLoading = false,
+                isError = false,
+                errorMessage = "",
+                articles = articles,
+                shareArticle = null,
+                showShareOption = false
+        ))
+    }
+
+    @Test
+    fun LoadErrorTest() {
+        whenever(repository.loadNews()).thenReturn(Single.error(Throwable("This is somekind of error")))
+        homeViewModel.processIntents(Observable.just(HomeIntent.InitialIntent))
+        verify(observer).onChanged(HomeViewState(
+                isLoading = true,
+                isError = false,
+                errorMessage = "",
+                articles = emptyList(),
+                showShareOption = false,
+                shareArticle = null
+        ))
+        verify(observer).onChanged(HomeViewState(
+                isLoading = false,
+                isError = true,
+                errorMessage = "This is somekind of error",
+                articles = emptyList(),
+                shareArticle = null,
+                showShareOption = false
+        ))
+    }
+
+    @Test
+    fun onNewsClickTest() {
+        val article = ArticlesItem("", "", ",", "", Source("", ""), "", "")
+        homeViewModel.processIntents(Observable.just(HomeIntent.ClickIntent(article)))
+        verify(observer).onChanged(HomeViewState(
+                isLoading = false,
+                isError = false,
+                errorMessage = "",
+                articles = emptyList(),
+                shareArticle = article,
+                showShareOption = true
+        ))
+    }
+
+}
